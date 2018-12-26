c77f32e5c07053363e6719e621654bebb82babeb
Jon Huynh <jhsoccer123@gmail.com>
2018-12-26 18:15:55 -0500

    Lesson 7 and 8 included here -- Controller files created and programmatic segue

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,33 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Jon Huynh on 12/26/18.
+//  Copyright © 2018 Jon Huynh. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
