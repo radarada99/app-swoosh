fe25157c352fe157888e0ddc6af1ab90eb30c6ea
Jon Huynh <jhsoccer123@gmail.com>
2018-12-27 15:49:58 -0500

    Added Player Struct and conditional button functionality

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Jon Huynh on 12/27/18.
+//  Copyright © 2018 Jon Huynh. All rights reserved.
+//
+
+import Foundation
+
+struct Player { // use struct whenever possible as opposed to class
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
